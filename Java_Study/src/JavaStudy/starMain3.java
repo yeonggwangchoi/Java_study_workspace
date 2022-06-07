ede6289e54b407f122e69296ac663c5b2bf04baa
yeonggwangchoi <dudrhkd7811@naver.com>
2022-06-07 18:55:07 +0900

    220607 commit

---
+++ Java_Study/src/JavaStudy/starMain3.java
@@ -0,0 +1,18 @@
+package JavaStudy;
+
+public class starMain3 {
+	public static void main(String[] args)
+	{
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(j<i)
+					System.out.print(" ");
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Java_Study/src/JavaStudy/starmain.java
@@ -0,0 +1,17 @@
+package JavaStudy;
+
+public class starmain {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				if(i+j>3)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+}
